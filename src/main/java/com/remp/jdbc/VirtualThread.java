17664849299413619aee84922131123454df3514
Stas <stasolsh@gmail.com>
2025-10-26 11:16:57 +0200

    Add Virtual Thread

--- src/main/java/com/remp/jdbc/VirtualThread.java
+++ src/main/java/com/remp/jdbc/VirtualThread.java
@@ -11,16 +11,17 @@ import java.util.Properties;
 import java.util.logging.Logger;
 import java.util.stream.IntStream;
 
-public class PlatformThread {
+public class VirtualThread {
     private final static String DB_URL = DBConfig.getDbUrl();
     private final static String DB_USER = DBConfig.getDbUser();
     private final static String DB_PASSWORD = DBConfig.getDbPassword();
-    private final static String queryStatement = "SELECT * FROM SH.CUSTOMERS WHERE CUST_ID = 49671";
-    private final static Logger LOGGER = Logger.getLogger("com.remp.jdbc.PlatformThread");
+    private static final String QUERY = "SELECT * FROM SH.CUSTOMERS WHERE CUST_ID = 49671";
+    private final static Logger LOGGER = Logger.getLogger("com.remp.jdbc.VirtualThread");
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String args[]) throws SQLException {
 
         OracleConnection connection = getOracleConnection();
+
         DatabaseMetaData dbmd = connection.getMetaData();
         System.out.println(STR."Driver Name: \{dbmd.getDriverName()}");
         System.out.println(STR."Driver Version: \{dbmd.getDriverVersion()}");
@@ -28,17 +29,15 @@ public class PlatformThread {
 
         Instant start = Instant.now();
 
-        // platform threads
-        var threads = IntStream.range(0, 1_500).mapToObj(i -> new Thread(() -> {
+        // virtual threads
+        var threads = IntStream.range(0, 1_500).mapToObj(i -> Thread.startVirtualThread(() -> {
             try {
-                performWork(connection);
+                doSQLWork(connection);
                 System.out.println(STR."Query #: \{i}");
             } catch (SQLException ex) {
                 LOGGER.severe(ex.getMessage());
             }
         })).toList();
-
-        threads.forEach(Thread::start);
 
         for (var thread : threads) {
             try {
@@ -68,10 +67,10 @@ public class PlatformThread {
         return (OracleConnection) pds.getConnection();
     }
 
-    private static void performWork(Connection conn) throws SQLException {
+    private static void doSQLWork(Connection conn) throws SQLException {
         conn.setAutoCommit(false);
         try (Statement statement = conn.createStatement();
-             ResultSet resultSet = statement.executeQuery(PlatformThread.queryStatement)) {
+             ResultSet resultSet = statement.executeQuery(VirtualThread.QUERY)) {
             while (resultSet.next()) {
                 System.out.println(new StringBuilder(resultSet.getString(1)).append(" ").append(resultSet.getString(2))
                         .append(" ").append(resultSet.getString(3)).append(" ").append(resultSet.getString(4))
